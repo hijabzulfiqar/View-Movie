237cce09042dde579c6efa67484c6bdd440106d6
Hijab Zulfiqar <hijabzulfiqar2001@gmail.com>
2024-04-03 11:36:25 +0500

    refactor:protected route

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -32,3 +36,4 @@ export default {
     // ...
   ]
 };
+
